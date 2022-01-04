c0d683e01ca1f89ee8c70013a0cf35907e27473a
Kumaresh Baksi <baksikumaresh@gmail.com>
2022-01-04 10:07:44 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('header').toggleClass('toggle');
+    });
+  
+    $(window).on('scroll load',function(){
+  
+      $('#menu').removeClass('fa-times');
+      $('header').removeClass('toggle');
+  
+      if($(window).scrollTop() > 0){
+        $('.top').show();
+      }else{
+        $('.top').hide();
+      }
+  
+    });
+  
+    // smooth scrolling 
+  
+    $('a[href*="#"]').on('click',function(e){
+  
+      e.preventDefault();
+  
+      $('html, body').animate({
+  
+        scrollTop : $($(this).attr('href')).offset().top,
+  
+      },
+        500, 
+        'linear'
+      );
+  
+    });
+  
+  });
+
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzzSW_L7PtiPDr3EWsk-sQ7FJhfeAG2FMou58tvkQizFMp672gzK06PRKHvGGs1Cs5k/exec'
+            const form = document.forms['google-sheet']
+          
+            form.addEventListener('submit', e => {
+              e.preventDefault()
+              fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+                .then(response => alert("Message sent successfully"))
+                .catch(error => console.error('Error!', error.message))
+            });
+  
